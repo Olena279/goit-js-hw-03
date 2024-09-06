8e3a8f10f11acfd3bd1f4f7438740f0d032e6b2f
Olenka <Riabova_o@ukr.net>
2024-09-06 22:09:06 +0300

    update js-tasks

--- js/task-1.js
+++ js/task-1.js
@@ -1,7 +1,8 @@
 `use strict`;
 function slugify(title) {
-  return title.join('-').length;
+  return title.toLowerCase().split(' ').join('-');
 }
+
 console.log(slugify('Arrays for beginners')); // "arrays-for-beginners"
 console.log(slugify('English for developer')); // "english-for-developer"
 console.log(slugify('Ten secrets of JavaScript')); // "ten-secrets-of-javascript"
